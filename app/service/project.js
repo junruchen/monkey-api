9302274f4b8b11f4d4ecf99f357b7f5603639278
陈俊汝 <chenjunru05@google.com>
2018-07-24 20:35:41 +0800

    Feat：项目初始化

---
+++ app/service/project.js
@@ -0,0 +1,11 @@
+const Service = require('egg').Service
+class ProjectService extends Service {
+  async get () {
+    const project = [{
+
+    }]
+    return project
+  }
+}
+
+module.exports = ProjectService
